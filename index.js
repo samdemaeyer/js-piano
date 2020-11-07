c66b1611043701312fe416cac90f90783367d373
Sam De Maeyer <sam_demaeyer@hotmail.com>
2020-11-07 20:59:19 +0000

    Add styles, game functinality and eslint

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-soundsUrls.forEach(({ note, audio }) => {
+const startBtn = document.getElementById('start-btn')
+const noteToPlay = document.getElementById('note-to-play')
+
+const startGame = ({ target }) => {
+  target.innerText === 'Start' && (target.innerText = 'Next')
+  const noteNr = Math.floor(Math.random() * notes.length)
+  noteToPlay.innerText = notes[noteNr].note
+  document.querySelector('.is-valid')?.classList.remove('is-valid')
+}
+
+startBtn.onclick = startGame
+
+notes.forEach(({ note, audio }) => {
   const btn = document.createElement('button')
   btn.classList.add('note', note)
-  btn.onclick = () => audio.cloneNode().play()
+  btn.onclick = (e) => {
+    audio.cloneNode().play()
+    if (note === noteToPlay.innerText) {
+      e.target.classList.add('is-valid')
+    } else {
+      e.target.classList.add('is-invalid')
+      setTimeout(() => e.target.classList.remove('is-invalid'), 350)
+    }
+  }
   document.querySelector('.piano-wrapper').append(btn)
 })
--- sounds.js
+++ sounds.js
@@ -1,123 +1,123 @@
-
-const soundsUrls = [
+// eslint-disable-next-line no-unused-vars
+const notes = [
   {
     audio: new Audio('assets/audio/notes/0A.wav'),
-    note: '0A'
+    note: '0A',
   },
   {
     audio: new Audio('assets/audio/notes/0A-sharp.wav'),
-    note: '#0A'
+    note: '#0A',
   },
   {
     audio: new Audio('assets/audio/notes/0B.wav'),
-    note: '0B'
+    note: '0B',
   },
   {
     audio: new Audio('assets/audio/notes/0C.wav'),
-    note: '0C'
+    note: '0C',
   },
   {
     audio: new Audio('assets/audio/notes/0C-sharp.wav'),
-    note: '#0C'
+    note: '#0C',
   },
   {
     audio: new Audio('assets/audio/notes/0D.wav'),
-    note: '0D'
+    note: '0D',
   },
   {
     audio: new Audio('assets/audio/notes/0D-sharp.wav'),
-    note: '#0D'
+    note: '#0D',
   },
   {
     audio: new Audio('assets/audio/notes/0E.wav'),
-    note: '0E'
+    note: '0E',
   },
   {
     audio: new Audio('assets/audio/notes/0F.wav'),
-    note: '0F'
+    note: '0F',
   },
   {
     audio: new Audio('assets/audio/notes/0F-sharp.wav'),
-    note: '#0F'
+    note: '#0F',
   },
   {
     audio: new Audio('assets/audio/notes/0G.wav'),
-    note: '0G'
+    note: '0G',
   },
   {
     audio: new Audio('assets/audio/notes/0G-sharp.wav'),
-    note: '#0G'
+    note: '#0G',
   },
   {
     audio: new Audio('assets/audio/notes/1A.wav'),
-    note: '1A'
+    note: '1A',
   },
   {
     audio: new Audio('assets/audio/notes/1A-sharp.wav'),
-    note: '#1A'
+    note: '#1A',
   },
   {
     audio: new Audio('assets/audio/notes/1B.wav'),
-    note: '1B'
+    note: '1B',
   },
   {
     audio: new Audio('assets/audio/notes/1C.wav'),
-    note: '1C'
+    note: '1C',
   },
   {
     audio: new Audio('assets/audio/notes/1C-sharp.wav'),
-    note: '#1C'
+    note: '#1C',
   },
   {
     audio: new Audio('assets/audio/notes/1D.wav'),
-    note: '1D'
+    note: '1D',
   },
   {
     audio: new Audio('assets/audio/notes/1D-sharp.wav'),
-    note: '#1D'
+    note: '#1D',
   },
   {
     audio: new Audio('assets/audio/notes/1E.wav'),
-    note: '1E'
+    note: '1E',
   },
   {
     audio: new Audio('assets/audio/notes/1F.wav'),
-    note: '1F'
+    note: '1F',
   },
   {
     audio: new Audio('assets/audio/notes/1F-sharp.wav'),
-    note: '#1F'
+    note: '#1F',
   },
   {
     audio: new Audio('assets/audio/notes/1G.wav'),
-    note: '1G'
+    note: '1G',
   },
   {
     audio: new Audio('assets/audio/notes/1G-sharp.wav'),
-    note: '#1G'
+    note: '#1G',
   },
   {
     audio: new Audio('assets/audio/notes/2A.wav'),
-    note: '2A'
+    note: '2A',
   },
   {
     audio: new Audio('assets/audio/notes/2A-sharp.wav'),
-    note: '#2A'
+    note: '#2A',
   },
   {
     audio: new Audio('assets/audio/notes/2B.wav'),
-    note: '2B'
+    note: '2B',
   },
   {
     audio: new Audio('assets/audio/notes/2C.wav'),
-    note: '2C'
+    note: '2C',
   },
   {
     audio: new Audio('assets/audio/notes/2C-sharp.wav'),
-    note: '#2C'
+    note: '#2C',
   },
   {
     audio: new Audio('assets/audio/notes/2D.wav'),
-    note: '2D'
+    note: '2D',
   },
 ]
